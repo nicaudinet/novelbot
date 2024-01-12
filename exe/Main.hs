4f9f7b22273dd71a1150037014b052d77b4adda6
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2024-01-12 10:26:29 +0100

    Fungen hello world app

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Graphics.UI.Fungen
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+    let winConfig = ((50,50), (500,300), "Hello world")
+        gameMap = colorMap 0.0 0.0 0.0 250 250
+        bindings = [(Char 'q', Press, \_ _ -> funExit)]
+    in funInit winConfig gameMap [] () () bindings (return()) Idle []
