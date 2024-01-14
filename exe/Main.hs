1c8d189a67d8ab1ab398eff394607abc60b6b7d0
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2024-01-14 12:03:32 +0100

    Group of robots bouncing and exploding
    - Use mapM and forM to replicate the robots
    - Use bitmaps for robot and explosion
    - Change the state of an object when it hits one of the vertical walls

--- exe/Main.hs
+++ exe/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Graphics.UI.Fungen hiding (Position)
 import Graphics.Rendering.OpenGL (GLdouble, GLfloat)
+import System.Random (randomRIO)
+import Control.Monad (forM_)
 
 type Name = String
 type Dimentions = Point2D
@@ -9,45 +11,52 @@ type Position = Point2D
 type Color = (GLfloat, GLfloat, GLfloat)
 
 width, height :: Int
-width = 400
-height = 400
+width = 800
+height = 800
 
 w, h :: GLdouble
 w = fromIntegral width
 h = fromIntegral height
 
-main :: IO ()
-main =
-    let
-        winConfig = ((50, 50), (width, height), "Hello world")
-        gameMap = colorMap 0.0 0.0 0.0 250 250
-        bindings = [(Char 'q', Press, \_ _ -> funExit)]
-        ball = objectGroup "ballGroup" [createBall]
-        room = objectGroup "roomGroup" createRoom
-    in
-        funInit
-            winConfig -- main window layout
-            gameMap -- background
-            [ball, room] -- object groups
-            () -- initial game state
-            () -- initial game attribute
-            bindings -- input bindings
-            gameCycle -- step action
-            (Timer 30) -- main loop timing
-            [] -- image files
+invisMagenta :: [FilePath] -> [(FilePath, InvList)]
+invisMagenta = map (\x -> (x, Just [(255, 0, 255)]))
 
-createBall :: GameObject ()
-createBall =
-    let
-        ballPic = Basic (Circle 5.0 0.0 1.0 0.0 Filled)
-    in
-        object
-            "ball" -- name
-            ballPic -- object picture
-            False -- asleep
-            (w/2, h/2) -- position
-            (-4,4) -- speed
-            () -- Object Attributes
+main :: IO ()
+main = do
+    let winConfig = ((50, 50), (width, height), "Hello world")
+    let gameMap = colorMap 0.0 0.0 0.0 250 250
+    let room = objectGroup "roomGroup" createRoom
+    robots <- objectGroup "robotGroup" <$> createRobots
+    let bindings = [(Char 'q', Press, \_ _ -> funExit)]
+    let bmpList = invisMagenta ["images/boom.bmp", "images/robot.bmp"]
+    funInit
+        winConfig -- main window layout
+        gameMap -- background
+        [room, robots] -- object groups
+        () -- initial game state
+        () -- initial game attribute
+        bindings -- input bindings
+        gameCycle -- step action
+        (Timer 30) -- main loop timing
+        bmpList -- image files
+
+----------
+-- INIT --
+----------
+
+createRobot :: Int -> IO (GameObject ())
+createRobot index = do
+    speed <- (,) <$> randomRIO (-5,5) <*> randomRIO (-5,5)
+    pure $ object
+        ("robot-" <> show index) -- name
+        (Tex (25,25) 1) -- object picture
+        False -- asleep
+        (w/2, h/2) -- position
+        speed -- speed
+        () -- Object Attributes
+
+createRobots :: IO [GameObject ()]
+createRobots = mapM createRobot [1..10]
 
 rectangleBound :: GLdouble -> GLdouble -> [Point2D]
 rectangleBound roomWidth roomHeight =
@@ -70,16 +79,16 @@ createRoom :: [GameObject ()]
 createRoom = [ wall1, wall2, wall3, wall4, wall5 ]
     where
         roomWidth :: GLdouble
-        roomWidth = 200
+        roomWidth = 600
 
         roomHeight :: GLdouble
-        roomHeight = 50
+        roomHeight = 150
 
         thickness :: GLdouble
-        thickness = 2
+        thickness = 5
 
         doorWidth :: GLdouble
-        doorWidth = 30
+        doorWidth = 50
 
         color :: Color
         color = (1.0, 1.0, 1.0) -- white
@@ -114,23 +123,34 @@ createRoom = [ wall1, wall2, wall3, wall4, wall5 ]
                 position = (w / 2 - roomWidth / 2 + (fst dimentions) / 2, h / 2 + roomHeight / 2)
             in createRectangle "wall5" dimentions position color
 
+-------------
+-- UPDATES --
+-------------
+
+explode :: GameObject s -> IOGame t s u v ()
+explode obj = do
+    replaceObject obj (updateObjectSize (100,100))
+    setObjectCurrentPicture 0 obj
+    setObjectSpeed (0,0) obj
+
+---------------
+-- GAME LOOP --
+---------------
+
 gameCycle :: IOGame () () () () ()
 gameCycle = do
-
-    -- Get the ball object
-    ball <- findObject "ball" "ballGroup"
-
-    -- Vertical wall collisions
-    wall1 <- findObject "wall1" "roomGroup"
-    wall3 <- findObject "wall3" "roomGroup"
-    vColl <- objectListObjectCollision [wall1, wall3] ball
-    when vColl (reverseXSpeed ball)
-
-    -- Horizontal wall collisions
-    wall2 <- findObject "wall2" "roomGroup"
-    wall4 <- findObject "wall4" "roomGroup"
-    wall5 <- findObject "wall5" "roomGroup"
-    hColl <- objectListObjectCollision [wall2, wall4, wall5] ball
-    when hColl (reverseYSpeed ball)
-
     showFPS TimesRoman24 (w - 40, 0) 1.0 0.0 0.0
+    robots <- getObjectsFromGroup "robotGroup"
+    forM_ robots $ \robot -> do
+        -- Vertical wall collisions
+        wall1 <- findObject "wall1" "roomGroup"
+        wall3 <- findObject "wall3" "roomGroup"
+        vColl <- objectListObjectCollision [wall1, wall3] robot
+        when vColl (explode robot)
+        -- Horizontal wall collisions
+        wall2 <- findObject "wall2" "roomGroup"
+        wall4 <- findObject "wall4" "roomGroup"
+        wall5 <- findObject "wall5" "roomGroup"
+        hColl <- objectListObjectCollision [wall2, wall4, wall5] robot
+        when hColl (reverseYSpeed robot)
+
